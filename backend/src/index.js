dbb56b51cddc05b0eb0e7c536cfadb639577fbb1
Neylanio <neylaniogomes@gmail.com>
2020-06-17 17:18:08 -0300

    Use parameters types

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { response } = require('express');
 
 const app = express();
+
+app.use(express.json());
 
 
 /**
@@ -13,34 +14,87 @@ const app = express();
  * DELETE: destroy especific element
  */
 
+
+/**
+ * Parameters types
+ * 
+ * Query params: Filters and pagination, ex: ?page=2
+ * Route params: Identify resources (UPDATE, DELETE)
+ * Request body: Body to create or to delete resource (JSON)
+ * 
+ */
+
+
+const arr = [
+    'Projeto 1',
+    'Projeto 2'
+];
+
+
+// Common GET 
+// app.get('/projects', (request, response) => {
+//     return response.json(arr);
+// });
+
+
+// //GET with Query params
 app.get('/projects', (request, response) => {
-    return response.json([
-        'Projeto 1',
-        'Projeto 2'
-    ]);
+
+    const { title } = request.query;
+
+    return arr.find(item => item == title) 
+            ? response.json(arr) 
+            : response.status(404).json({message: "Not Found Element"});
+
 });
+
+
+//GET with Route params
+app.get('/projects/:title', (request, response) => {
+
+    const { title } = request.params;
+
+    return arr.find(item => item == title) 
+            ? response.json(arr) 
+            : response.status(404).json({message: "Not Found Element"});
+
+});
+
 
 app.post('/projects', (request, response) => {
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+
+    const { title } = request.body;
+
+    arr.push(title);
+
+    return response.json(arr);
 });
 
-app.put('/projects/:id', (request, response) => {
-    return response.json([
-        'Projeto 4',
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+app.put('/projects/:title', (request, response) => {
+
+    const { title } = request.params;        
+    const { content } = request.body;
+
+    if(~arr.indexOf(title)){
+        arr[arr.indexOf(title)] = content;   
+        return response.json(arr);
+    }else{
+        return response.status(404).json({message: "Not Found Element"});
+    }
 });
 
-app.delete('/projects/:id', (request, response) => {
-    return response.json([
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+
+app.delete('/projects/:title', (request, response) => {
+
+    const { title } = request.params;
+
+    if(~arr.indexOf(title)){
+        delete arr[arr.indexOf(title)];
+        return response.json(arr);
+    }else{
+        return response.status(404).json({message: "Not Found Element"});
+    }
+
 });
 
 
