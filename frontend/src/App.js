e87f7c987e109ea4a8c0624b7e1f58aa0a18fd95
Neylanio <neylaniogomes@gmail.com>
2020-06-19 17:23:00 -0300

    Use properties in Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,18 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header />
-      <Header />
+      <Header title="HomePage">
+        <ul>
+          <li>HomePage 1</li>
+          <li>HomePage 2</li>
+        </ul>
+      </Header>
+      <Header title="Project">
+        <ul>
+          <li>Project 3</li>
+          <li>Project 4</li>
+        </ul>
+      </Header>
     </>
   );
 }
