be7b107b248d84975ac990f0be8ebb02ab82cfba
Neylanio <neylaniogomes@gmail.com>
2020-06-25 21:34:10 -0300

    feat: Start mobile project

---
+++ mobile/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Algo aqui</Text>
+    </View>
+  )
+}
+
+export default App;
