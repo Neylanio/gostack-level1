374d8b86b21a7e477a56ca2063c17bc13a0b7100
Neylanio <neylaniogomes@gmail.com>
2020-06-25 23:40:27 -0300

    feat(mobile): List repositories

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, Text, StyleSheet, StatusBar, FlatList } from 'react-native';
+
+import api from './services/api';
 
 const App = () => {
+
+  const [ repositories, setRepositories ] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      console.log(response.data);
+      setRepositories(response.data);
+    });
+  }, []);
+
+
   return (
-    <View>
-      <Text>Algo aqui</Text>
-    </View>
-  )
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+      <SafeAreaView style={styles.container}>
+        <FlatList 
+          data={repositories}
+          keyExtractor={repo => repo.id}
+          renderItem={( { item: repo } ) => (
+            <Text style={styles.repository}>{repo.title}</Text>
+          )}
+        />
+      </SafeAreaView>
+    </>  
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7159c1",
+  },
+  title: {
+    fontSize: 40,
+    color: "#FFF",
+    fontWeight: "bold"
+  },
+  repository: {
+    fontSize: 30,
+    color: "#FFF",
+    marginTop: 10,
+  }
+});
 
 export default App;
